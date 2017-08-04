65a922c9d2f4757c672b25dda296354a89fbb756
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-08-04 23:14:33 +0300

    Added description of BaseModel

--- base_model.py
+++ base_model.py
@@ -1,1 +1,17 @@
+class BaseModel(object):
 
+    def save(self, *args, **kwargs):
+        """ Save model. """
+        raise NotImplementedError
+
+    def load(self, *args, **kwargs):
+        """ Load model from hard drive. """
+        raise NotImplementedError
+
+    def fit(self, *args, **kwargs):
+        """ Fit model on input data. """
+        raise NotImplementedError
+
+    def predict(self, *args, **kwargs):
+        """ Get predictions of model on input data. """
+        raise NotImplementedError
